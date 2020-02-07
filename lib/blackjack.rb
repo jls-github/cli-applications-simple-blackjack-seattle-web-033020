9cf5f89f1dc634dcc338ac1987d2eb742fe17d08
John Souza <souzajohnlee@gmail.com>
2020-02-07 18:06:01 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -6,16 +6,16 @@ def deal_card
   rand(11)
 end
 
-def display_card_total
-  # code #disp
+def display_card_total(total)
+  puts "Your cards add up to #{total}."
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay."
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets.chomp
 end
 
 def end_game
